afca96c592d7120c91ce71b1be6326264380bc91
Bosticardo-Andrea <90908021+Bosticardo-Andrea@users.noreply.github.com>
2021-11-04 15:39:23 +0100

    Add files via upload

---
+++ Python/Compiti/Es_010_Compito.py
@@ -0,0 +1,4 @@
+#Scrivi un programma in Python che permetta all’utente di inserire un numero intero e una stringa: concatena la stringa con sé stessa tante volte quante il numero e stampa il risultato.
+x  = int(input("Inserisci il numero: "))
+frase = input("Inserisci una stringa: ")
+print(x*(f"{frase}. "))
