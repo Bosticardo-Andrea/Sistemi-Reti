11d1311e941a76374c9b302f4ffa920b411ea0e3
Bosticardo-Andrea <90908021+Bosticardo-Andrea@users.noreply.github.com>
2021-11-24 09:31:59 +0100

    Add files via upload

--- Python/Compiti/Es_020_Compito.py
+++ Python/Compiti/Es_020_Compito.py
@@ -1,5 +1,4 @@
 #Creare una lambda function che ritorni True se una stringa è palindroma, False altrimenti.
-palindroma = lambda frase :  (True if str(frase) == str(frase)[::-1] else False)
-
+palindroma = lambda frase : str(frase) == str(frase)[::-1]
 frase = input("inserisci una stringa: ")
 print(palindroma(frase))
