f5dba193f0589796c42b531a771fa4b0b1d61c9f
Bosticardo-Andrea <90908021+Bosticardo-Andrea@users.noreply.github.com>
2022-04-05 22:29:49 +0200

    don't copy

--- Python/Pentamestre/Es/Es_010_ClientUDP.py
+++ Python/Pentamestre/Es/Es_010_ClientUDP.py
@@ -2,5 +2,7 @@ import socket
 #socket.AF_INET = IPV4
 #socket.SOCK_DGRAM = UDP
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-stringa = "Ciao"
-s.sendto(stringa,("127.0.0.1",5000))
+while True:
+    stringa = input("Inserisci una stringa: ")
+    if (stringa == "Exit") | (stringa == "exit"):break
+    s.sendto(stringa.encode(),("127.0.0.1",5000))
--- Python/Pentamestre/Es/Es_010_ServerUDP.py
+++ Python/Pentamestre/Es/Es_010_ServerUDP.py
@@ -2,6 +2,10 @@ import socket
 #socket.AF_INET = IPV4
 #socket.SOCK_DGRAM = UDP
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.bind(("127.0.0.1",5000)) # solo sui server
-dati,id_client = s.recv(4096)#4096 = dimensione in byte del buffer
-print(f"{dati.decode()} ricevuti da {id_client}")
+s.bind(("192.168.0.129",5000)) # solo sui server
+while True:
+    dati,address= s.recvfrom(4096)#4096 = dimensione in byte del buffer
+    print(f"{dati.decode()}\t inviato da: {address}")
+    stringa = input("Inserisci una stringa: ")
+    if (stringa == "Exit") | (stringa == "exit"):break
+    s.sendto(stringa.encode(),(address[0],address[1]))
