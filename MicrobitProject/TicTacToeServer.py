e71e0652c2386f7ddeee8f4d02e6dc1bf88af6fe
Bosticardo-Andrea <90908021+Bosticardo-Andrea@users.noreply.github.com>
2022-05-12 20:05:46 +0200

    Add files via upload

--- MicrobitProject/TicTacToeServer.py
+++ MicrobitProject/TicTacToeServer.py
@@ -10,22 +10,21 @@ class MyThread(Thread):
         self.g2 = g2
         self.tipo = None
         self.giocatori = giocatori
+        self.dizioCoordinate = {0:[61.5,61.5],1:[196.5,61.5],2:[331.5,61.5],3:[61.5,196.5],4:[196.5,200],5:[331.5,196.5],6:[61.5,330.5],7:[196.5,330.5],8:[331.5,330.5]}
     def run(self):
         pygame.init()
         size = (400,475)
         fnt2 = pygame.font.SysFont("Times New Roman", 25)
         fnt = pygame.font.SysFont("Times New Roman", 110)
+        fnt3 = pygame.font.SysFont("Times New Roman", 50)
         screen = pygame.display.set_mode(size)
-        dizioCoordinate = {0:(37.5,0),1:(172.5,0),2:(307.5,0),3:(37.5,135),4:(172.5,135),5:(307.5,135),6:(37.5,270),7:(172.5,270),8:(307.5,270)}
-        """surf_text = fnt.render(self.g2, True, (0, 0, 0))
-        screen.blit(surf_text, (10,50))"""
         while self.runnig:
             screen.fill((255,255,255))
             pygame.draw.rect(screen,(0,0,0),(130,10,5,380))
             pygame.draw.rect(screen,(0,0,0),(265,10,5,380))
             pygame.draw.rect(screen,(0,0,0),(10,130,380,5))
             pygame.draw.rect(screen,(0,0,0),(10,265,380,5))
-            for chiave in dizioCoordinate:
+            for chiave in self.dizioCoordinate:
                 if self.griglia[chiave] == " ":
                     surf_text = fnt.render(str(chiave), True, (0,0,0))
                 else:
@@ -33,7 +32,7 @@ class MyThread(Thread):
                         surf_text = fnt.render(self.griglia[chiave], True, (255,0,0))
                     else:
                         surf_text = fnt.render(self.griglia[chiave], True, (0,255,0))
-                screen.blit(surf_text, dizioCoordinate[chiave])
+                screen.blit(surf_text, (self.dizioCoordinate[chiave][0]-40,self.dizioCoordinate[chiave][1]-65))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: sys.exit()
             TestoG1 = fnt2.render("".join([self.g1," = ",self.giocatori[self.g1]]), True, (255,0,0))
@@ -43,17 +42,19 @@ class MyThread(Thread):
             if self.tipo != None:
                 screen.fill((255,255,255))
                 if self.tipo == 3:
-                    risultato = fnt2.render("Pareggio", True, (0,255,0))
+                    risultato = fnt3.render("Pareggio", True, (0,0,0))
                 if self.tipo == 1:
-                    risultato = fnt2.render(f"vince: {self.g1}", True, (0,255,0))
+                    pygame.draw.line(screen, (0,0,0), self.inizio, self.end, 10)
+                    risultato = fnt3.render(f"Hai perso:", True, (255,0,0))
                 if self.tipo == 2:
-                    risultato = fnt2.render(f"Vince {self.g2}", True, (0,255,0))
-                screen.blit(risultato, (10,200))
+                    pygame.draw.line(screen, (0,0,0), self.inizio, self.end, 10)
+                    risultato = fnt3.render(f"Hai vinto", True, (0,255,0))
+                screen.blit(risultato, (10,200)) 
             pygame.display.flip() 
     def stop(self,tipo):
         self.tipo = tipo
-    def aggiornaGriglia(self,griglia):
-        self.griglia = griglia
+    def linea(self,s,e):
+        self.inizio,self.end = self.dizioCoordinate[s],self.dizioCoordinate[e]
 def connessione():
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.bind(("127.0.0.1",8000))
@@ -66,14 +67,11 @@ def controllo(x,g,griglia,giocatori):
     """Si effettua il controllo del numero inserito:
      - Nel caso la cella sia occupata
      - Nel caso il numero non si corretto < 0 o > 8"""
-    while((x < 0 or x > 8)):
-        x = int(input("Numero non valido:"))
     while((griglia[x] != " ")):       
         print("Cella occupata")
         x = int(input("Inserici mossa:"))
-        while((x < 0 or x > 8)):
-            x = int(input("Numero non valido:"))
     griglia[x] = giocatori[g]   
+    return x
 def disegnaGriglia(griglia,giocatori,g1,g2):
     """Disegno la griglia con i giocatori che si sfidano"""
     print(f"\n{g1} [{giocatori[g1]}] vs {g2} [{giocatori[g2]}]\n")
@@ -83,24 +81,32 @@ def disegnaGriglia(griglia,giocatori,g1,g2):
     print(f"---+---+---")
     print(f" {griglia[6]} | {griglia[7]} | {griglia[8]} ")
     print("\n")
-def vittoria(griglia):
+def vittoria(griglia,disegno):
     """controllo tutte i  possibili casi in cui si puó vincere"""
     vittoria = False
     if ((griglia[0]==griglia[1]==griglia[2]) & (griglia[0]!=" ")):
+        disegno.linea(0,2)
         vittoria = True
     elif ( (griglia[3]==griglia[4]==griglia[5]) & (griglia[3]!=" ")):
-       vittoria = True
+        disegno.linea(3,5)
+        vittoria = True
     elif ( (griglia[6]==griglia[7]==griglia[8]) & (griglia[6]!=" ")):
+        disegno.linea(6,8)
         vittoria = True
     elif ((griglia[0]==griglia[3]==griglia[6]) & (griglia[0]!=" ")):
+        disegno.linea(0,6)
         vittoria = True
     elif( (griglia[1]==griglia[4]==griglia[7]) & (griglia[1]!=" ")):
+        disegno.linea(1,7)
         vittoria = True
     elif( (griglia[2]==griglia[5]==griglia[8]) & (griglia[2]!=" ")):
+        disegno.linea(2,8)
         vittoria = True
     elif ((griglia[0]==griglia[4]==griglia[8]) & (griglia[0]!=" ")):
+        disegno.linea(0,8)
         vittoria = True 
     elif(( griglia[2]==griglia[4]==griglia[6]) & (griglia[2]!=" ")):
+        disegno.linea(2,6)
         vittoria = True
     return vittoria   
 def main():
@@ -118,34 +124,34 @@ def main():
         print(f"{G1}")
         print(f"Tocca a: {G1}")
         m = int(input("Inserici mossa: "))
+        m = controllo(m,G1,griglia,giocatori)
         connection.sendall(str(m).encode())
-        controllo(m,G1,griglia,giocatori)
         os.system('cls')
         disegnaGriglia(griglia,giocatori,G1,G2)
-        if(vittoria(griglia)):
+        if(vittoria(griglia,disegno)):
             print(f"Ha vinto {G1}")
-            disegno.stop(1)
+            disegno.tipo = 1
             break
         if(conta <= 8): 
             conta += 1
         else: 
-            disegno.stop(3)
+            disegno.tipo = 3
             break
         print(f"{G2}")
         print(f"Tocca a: {G2}")
         print("Attendi....")
         m = int(connection.recv(4096).decode())
-        controllo(m,G2,griglia,giocatori)
+        griglia[m] = giocatori[G2]
         os.system('cls')
         disegnaGriglia(griglia,giocatori,G1,G2)
-        if(vittoria(griglia)):
+        if(vittoria(griglia,disegno)):
             print(f"Ha vinto {G2}")
-            disegno.stop(2)
+            disegno.tipo = 2
             break
         if(conta <= 8): 
             conta += 1
         else: 
-            disegno.stop(3)
+            disegno.tipo = 3
             break 
 if __name__=="__main__":
     main()
